5583d5d38f709f032f193de38bce4fc91d7b2526
Jun Kawa <jun.kawa@compositeapps.net>
2018-06-12 11:41:38 -0700

    +example binding

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    public hello;
 
-    constructor() { }
+    constructor() {
+        this.hello;
+    }
 
     ngOnInit() {
 
